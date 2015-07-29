fabd6f3df423113c6a9c37024cff22ddcb7ad62c
Nagisa Furukawa <dgbrl58@gmail.com>
2015-07-28 21:16:30 -0500

    Update articlecontroller.rb

--- articlecontroller.rb
+++ articlecontroller.rb
@@ -20,6 +20,23 @@ def create
      render 'new'
   end
 end
+
+def update
+  @article = Article.find(params[:id])
+  
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+   end
+end
+
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+
+  redirect_to articles_path
+end
  
 private
   def article_params
